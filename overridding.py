5ea5b165cbfeae5279eb8133eafbd527b8e3b1c2
Prava12345 <160598367+Prava12345@users.noreply.github.com>
2024-05-04 13:39:57 +0530

    Delete overridding.py

--- overridding.py
+++
@@ -1,8 +0,0 @@
-class addition:
-                  def add(self,x,y):
-                    print(x+y)
-               class child(addition):
-                  def add(self,x,y,z):
-                    print(x+y+z)
-               i=child()
-               i.add(5,6,7)  
